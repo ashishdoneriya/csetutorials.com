1026fae3fb03fbd9cea33e7967ebaaeaefd2ff0b
Ashish Doneriya <ashishdoneriya@gmail.com>
2025-05-21 01:15:31 +0530

    Website changes

---
+++ commit.sh
@@ -0,0 +1,5 @@
+GIT_COMMITTER_NAME="Ashish Doneriya"
+GIT_COMMITTER_EMAIL="ashishdoneriya@gmail.com"
+git add -A
+git commit --author="Ashish Doneriya <ashishdoneriya@gmail.com>" -m "Website changes"
+git push origin main
